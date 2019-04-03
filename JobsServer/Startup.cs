319193af5dd7a48517418ec2e809508c72ef23f7
nan gong <gnsilence@outlook.com>
2019-04-03 16:55:13 +0800

    添加部分参数备注说明

--- JobsServer/Startup.cs
+++ JobsServer/Startup.cs
@@ -143,13 +143,13 @@ namespace JobsServer
                             new MySqlStorageOptions
                             {
                                 TablePrefix="hangfire",
-                                TransactionIsolationLevel = IsolationLevel.ReadCommitted,
-                                QueuePollInterval = TimeSpan.FromSeconds(1),//检测频率，秒级任务需要配置短点，一般任务可以配置默认时间
-                                JobExpirationCheckInterval = TimeSpan.FromHours(1),
-                                CountersAggregateInterval = TimeSpan.FromMinutes(5),
-                                PrepareSchemaIfNecessary = false,
-                                DashboardJobListLimit = 50000,
-                                TransactionTimeout = TimeSpan.FromMinutes(1),
+                                TransactionIsolationLevel = IsolationLevel.ReadCommitted,//实物隔离级别，默认为读取已提交
+                                QueuePollInterval = TimeSpan.FromSeconds(1),//队列检测频率，秒级任务需要配置短点，一般任务可以配置默认时间
+                                JobExpirationCheckInterval = TimeSpan.FromHours(1),//作业到期检查间隔（管理过期记录）。默认值为1小时
+                                CountersAggregateInterval = TimeSpan.FromMinutes(5),//聚合计数器的间隔。默认为5分钟
+                                PrepareSchemaIfNecessary = true,//设置true，则会自动创建表
+                                DashboardJobListLimit = 50000,//仪表盘作业列表展示条数限制
+                                TransactionTimeout = TimeSpan.FromMinutes(1),//事务超时时间，默认一分钟
                             })).UseConsole(new ConsoleOptions()
                             {
                                 BackgroundColor = "#000079"
